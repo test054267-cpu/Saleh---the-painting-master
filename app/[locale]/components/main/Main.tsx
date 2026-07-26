8c2ced18e3ec09c3ac21f13dbce1e457283a569a
test054267-cpu <test054267@gmail.com>
2026-07-26 22:59:35 +0300

    Add files via upload

--- app/[locale]/components/main/Main.tsx
+++ app/[locale]/components/main/Main.tsx
@@ -6,7 +6,7 @@ const Main = () => {
   const t = useTranslations();
 
   return (
-    <main className="section h-dvh flex justify-center items-center px-10 pt-20">
+    <main className="section h-dvh flex justify-center items-center px-10 pt-40 lg:pt-20">
       <div className="w-150 flex flex-col items-center gap-10 text-center">
         <h1 className="text-3xl leading-10 font-bold text-shadow-lg relative z-20 text-white">
           {t("main.title")}
